5cc0d71c6a3ec22e33c543de4b52627f3ca9c673
MrLotU <j.koopman@jarict.nl>
2017-10-11 10:46:16 +0200

    Added images, started on UI

---
+++ NasAPP/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  NasAPP
+//
+//  Created by Jari Koopman on 09/10/2017.
+//  Copyright © 2017 JarICT. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    //MARK: Outlets
+    
+    @IBOutlet weak var homeButton: UIButton!
+    @IBOutlet weak var roverButton: UIButton!
+    @IBOutlet weak var eyeButton: UIButton!
+    @IBOutlet weak var asteroidButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+//MARK: - Navigation
+extension MainViewController {
+    
+    @objc func roverButtonPressed() {
+        
+    }
+
+}
+
